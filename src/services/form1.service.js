a908e4d9cbac062bd385bbbc7f559e5af47c51a3
“Giovanni” <giovannitakeshisesta@gmail.com>
2022-05-20 00:00:21 +0200

    .

--- src/services/form1.service.js
+++ src/services/form1.service.js
@@ -6,3 +6,6 @@ export const create   = (data) => http.post ('/form1',data)
 export const findAll  = ()     => http.get  ('/form1' )
 export const findById = (id)   => http.get  (`/form1/${id}`)
 export const findByIdAndUpdate = (id,data) => http.patch(`/form1/${id}`,data)
+
+
+export const create2   = (data) => http.post ('/form4',data)
--- src/views/Page2.js
+++ src/views/Page2.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import Form4 from '../components/Forms/Form4'
 
 export default function Page2() {
   return (
-    <div>Page2</div>
+    <div>
+      <h1>Page2</h1>
+      <div className='formDiv'> 
+        <Form4 /> 
+      </div>    
+    </div>
   )
 }
